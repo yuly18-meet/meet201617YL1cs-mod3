31568978649298fdeb60250d2bac4e4bf2f12fdf
Ted <golfit@mit.edu>
2016-12-06 14:01:08 -0500

    Update TestUserAccount.py
    Changed import to pull class from module.  Fixed typo in UserAccount name.  Also, removed space that was confusing students regarding where to put code.

--- TestUserAccount.py
+++ TestUserAccount.py
@@ -1,11 +1,10 @@
 #This script performs some simple tests on the UserAccount class.
-import UserAccount
+from UserAccount import UserAccount
 
 #Three things are missing from the line below - fill them in
-my_user=UserAcount(  ,  ,  )
+my_user=UserAccount(  ,  ,  )
 
 #Call the print_secret method (function) - it takes one input - a guess for the password.
-
 #Use the wrong password as input here
 my_user.
 
