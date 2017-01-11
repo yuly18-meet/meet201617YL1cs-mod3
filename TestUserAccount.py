8846cdc5bdb7fbc9c8dc4464244698e8322f8ceb
yuly18-meet <Yulybarshira@gmail.com>
2017-01-11 19:26:25 +0200

    made taste for UserAccount

--- TestUserAccount.py
+++ TestUserAccount.py
@@ -1,12 +1,8 @@
 #This script performs some simple tests on the UserAccount class.
 from UserAccount import UserAccount
 
-#Three things are missing from the line below - fill them in
-my_user=UserAccount(  ,  ,  )
 
-#Call the print_secret method (function) - it takes one input - a guess for the password.
-#Use the wrong password as input here
-my_user.
+user= UserAccount("yuly1234","1234","you did it!")
+user.print_secret(1234)
 
-#Use the right password here
-my_user.
+user.print_secret("1234")
