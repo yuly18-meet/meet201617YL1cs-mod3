876fc0513417f78ca5c1cbb7d2b6c98f7a43d61a
yuly18-meet <Yulybarshira@gmail.com>
2017-01-11 20:02:14 +0200

    made a class

---
+++ student.py
@@ -0,0 +1,16 @@
+class Student:
+    
+    def __init__(self, name, hometown, age, height):
+
+        self.name = name
+        self.hometown = hometown
+        self.age = age
+        self.height = height
+
+     def print_summary
+
+         print(self.name)
+         print(self.hometown)
+         print(self.age)
+         print(self.height)
+    
